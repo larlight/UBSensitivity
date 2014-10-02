537e2e710a3b909d01ec50e21a0443976e806ec7
David Kaleko <kaleko@nevis.columbia.edu>
2014-10-02 13:50:29 -0500

    fixing namespace issue

--- core/LEE/XSecScaling.hh
+++ core/LEE/XSecScaling.hh
@@ -26,70 +26,74 @@
    User defined class XSecScaling ... these comments are used to generate
    doxygen documentation!
  */
-class XSecScaling{
-
-public:
-
-  /// Default constructor
-  XSecScaling()
-  {
-    _my_xsec = 0;
-    _MB_xsec = 0;
-    _xsec_ratio = 0;
-    _my_xsec_input_filename = "";
-    _my_xsec_TGraph_name = "";
-    _MB_xsec_input_filename = "";
-    _MB_xsec_TGraph_name = "";
-    _my_ntargetspergram = GEO::UB_TARGETS_PER_GRAM;
-    _MB_ntargetspergram = GEO::MINIBOONE_TARGETS_PER_GRAM;
+namespace ubsens{
+  
+  class XSecScaling{
+    
+  public:
+    
+    /// Default constructor
+    XSecScaling()
+    {
+      _my_xsec = 0;
+      _MB_xsec = 0;
+      _xsec_ratio = 0;
+      _my_xsec_input_filename = "";
+      _my_xsec_TGraph_name = "";
+      _MB_xsec_input_filename = "";
+      _MB_xsec_TGraph_name = "";
+      _my_ntargetspergram = GEOCONSTANTS::UB_TARGETS_PER_GRAM;
+      _MB_ntargetspergram = GEOCONSTANTS::MINIBOONE_TARGETS_PER_GRAM;
+    };
+    
+    /// Default destructor
+    virtual ~XSecScaling(){};
+    
+    void SetMyInputFile(std::string filename_and_path)
+    { _my_xsec_input_filename = filename_and_path; }
+    
+    void SetMBInputFile(std::string filename_and_path)
+    { _MB_xsec_input_filename = filename_and_path; }
+    
+    void SetMyInputGraphName(std::string in_graph)
+    { _my_xsec_TGraph_name = in_graph; }
+    
+    void SetMBInputGraphName(std::string in_graph)
+    { _MB_xsec_TGraph_name = in_graph; }
+    
+    
+    //optional. default is microboone
+    void SetMyNTargetsPerGram(double value) { _my_ntargetspergram = value; }
+    
+    //optional. default is MB
+    void SetMBNTargetsPerGram(double value) { _MB_ntargetspergram = value; }
+    
+    bool LoadInputGraphs();
+    
+    void ComputeXSecRatio();
+    
+    TGraph* GetXSecRatio(){ return _xsec_ratio; }
+    
+    
+  protected:
+    
+    //cross section graph for your detector
+    TGraph *_my_xsec;
+    //cross section graph for miniboone
+    TGraph *_MB_xsec;
+    //division of the two cross section graphs
+    TGraph *_xsec_ratio;
+    
+    std::string _my_xsec_input_filename;
+    std::string _my_xsec_TGraph_name;
+    std::string _MB_xsec_input_filename;
+    std::string _MB_xsec_TGraph_name;
+    
+    double _my_ntargetspergram;
+    double _MB_ntargetspergram;
   };
-
-  /// Default destructor
-  virtual ~XSecScaling(){};
-
-  void SetMyInputFile(std::string filename_and_path)
-  { _my_xsec_input_filename = filename_and_path; }
-
-  void SetMBInputFile(std::string filename_and_path)
-  { _MB_xsec_input_filename = filename_and_path; }
-
-  void SetMyInputGraphName(std::string in_graph)
-  { _my_xsec_TGraph_name = in_graph; }
-
-  void SetMBInputGraphName(std::string in_graph)
-  { _MB_xsec_TGraph_name = in_graph; }
-
-
-  //optional. default is microboone
-  void SetMyNTargetsPerGram(double value) { _my_ntargetspergram = value; }
-
-  //optional. default is MB
-  void SetMBNTargetsPerGram(double value) { _MB_ntargetspergram = value; }
-
-  bool LoadInputGraphs();
-
-  void ComputeXSecRatio();
-
-  TGraph* GetXSecRatio(){ return _xsec_ratio; }
-
-
-protected:
-
-  //cross section graph for your detector
-  TGraph *_my_xsec;
-  //cross section graph for miniboone
-  TGraph *_MB_xsec;
-  //division of the two cross section graphs
-  TGraph *_xsec_ratio;
-
-  std::string _my_xsec_input_filename;
-  std::string _my_xsec_TGraph_name;
-  std::string _MB_xsec_input_filename;
-  std::string _MB_xsec_TGraph_name;
-
-  double _my_ntargetspergram;
-  double _MB_ntargetspergram;
-};
+  
+}//end namespace ubsens
 
 #endif
 /** @} */ // end of doxygen group 
