8c03c40316d1f9f697b132270f706dcb512bd25d
David Kaleko <kaleko@nevis.columbia.edu>
2014-10-14 16:30:15 -0500

    Class that returns a random true neutrino energy given a lepton energy, pulled from a slice of a th2f from a fullosc file

---
+++ core/LEE/NuLeptECorrelation.cc
@@ -0,0 +1,76 @@
+#ifndef NULEPTECORRELATION_CC
+#define NULEPTECORRELATION_CC
+
+#include "NuLeptECorrelation.hh"
+
+namespace ubsens{
+
+  bool NuLeptECorrelation::LoadInputTH2F(){
+
+    _my_fosc_filename = "/Users/davidkaleko/Data/LEE/combined_ntuple_470m_nu_fosc_CC_nu_lept_energycorrelation.root";
+    _my_th2f_name = "fosc_isCC_enugen_vs_Elep";
+
+    util::PlotReader::GetME()->Reset();
+
+    util::PlotReader::GetME()->SetFileName(_my_fosc_filename);
+    util::PlotReader::GetME()->SetPlotName(_my_th2f_name);
+    _my_th2f = util::PlotReader::GetME()->GetTH2F();
+
+    return true;
+  }
+
+
+  double NuLeptECorrelation::NuEFromLeptE(double lept_e){
+    
+    //For good measure, reset slice before starting
+    if( _my_tmp_slice )
+      _my_tmp_slice->Reset();
+ 
+    //check that _my_th2f is loaded, etc
+    if( !_my_th2f ){
+      std::ostringstream msg;
+      msg << "<<" << _classname <<"::"<< __FUNCTION__ <<">> "
+	  << "ERROR: You need to load in the TH2F from the fullosc file "
+	  << "if you want to correlate lepton energy with nu energy!"
+	  << std::endl;
+      throw LEEException(msg.str());
+      return -1;
+    }
+
+    //Find the x-bin # that corresponds to this lepton energy
+    Int_t bin = _my_th2f->GetXaxis()->FindBin(lept_e);
+
+    //Draw 1D projection of this bin from 2D histogram
+    _my_tmp_slice = _my_th2f->ProjectionY("_my_tmp_slice",bin,bin);
+    
+    //Pull random from this 1D distribution
+    double generated_nu_E = _my_tmp_slice->GetRandom();
+
+    //Return that random value
+    return generated_nu_E;
+
+  }
+
+  void NuLeptECorrelation::WritePlots(){
+   
+    //check that _my_th2f is loaded, etc
+    if( !_my_th2f ){
+      std::ostringstream msg;
+      msg << "<<" << __FUNCTION__ <<">> "
+	  << "ERROR: You need to load in the TH2F from the fullosc file "
+	  << "if you want to correlate lepton energy with nu energy!"
+	  << std::endl;
+      throw LEEException(msg.str());
+    }
+    
+    try{
+      util::PlotWriter::GetME()->Write(_my_th2f,_classname.c_str());
+    }
+    catch (LEEException &e) {
+      std::cout<<"Caught an exception! It is "<<e.what()<<std::endl;
+    }
+  
+  }
+  
+}//end namespace ubsens
+#endif
---
+++ core/LEE/NuLeptECorrelation.hh
@@ -0,0 +1,81 @@
+/**
+ * \file NuLeptECorrelation.hh
+ *
+ * \ingroup LEE
+ * 
+ * \brief This class reads in a fullosc file, makes a TH2F showing the
+ * correlation between lepton energy and true neutrino energy, then has
+ * a function that takes in lepton energy and draws randomly to spit out
+ * a true neutrino energy (or vice versa).
+ *
+ * @author davidkaleko
+ */
+
+/** \addtogroup LEE
+
+    @{*/
+#ifndef NULEPTECORRELATION_HH
+#define NULEPTECORRELATION_HH
+
+#include <iostream>
+#include <sstream>
+#include "LEEException.hh"
+#include "Utilities/PlotReader.hh"
+#include "Utilities/PlotWriter.hh"
+#include "FMWKBase/Message.hh"
+#include "TH2.h"
+/**
+   \class NuLeptECorrelation
+   User defined class NuLeptECorrelation ... these comments are used to generate
+   doxygen documentation!
+ */
+
+namespace ubsens{
+  
+  class NuLeptECorrelation{
+    
+  public:
+    
+    /// Default constructor
+    NuLeptECorrelation()
+    {
+      _my_fosc_filename = "";
+      _my_th2f_name = "";
+      _my_th2f = 0;
+      _my_tmp_slice = 0;
+      _classname = "NuLeptECorrelation";
+    };
+    
+    /// Default destructor
+    virtual ~NuLeptECorrelation(){};
+    
+    /// NOTE: input TH2F needs to be True neutrino energy (y-axis)
+    /// vs. True lepton energy (x-axis). This is normally gotten from
+    /// a fosc file. I took the fosc file, made a TH2F, and saved it to
+    /// a separate file, for cleanliness.
+    bool LoadInputTH2F();
+
+    /// Function that uses the input TH2F to return a true neutrino energy
+    /// from an input lepton energy. It takes a slice of the TH2F corresponding
+    /// to that lepton energy, then draws a random number from that distribution.
+    double NuEFromLeptE(double lept_e);
+
+    /// Function to save the TH2F that was used to the output plots file
+    /// (uses PlotWriter utility). Useful for debugging or cross-checks
+    /// or making powerpoint slides.
+    void WritePlots();
+
+  protected:
+    
+    std::string _my_fosc_filename;
+    std::string _my_th2f_name;
+    TH2F *_my_th2f;
+    TH1D *_my_tmp_slice;
+    
+    std::string _classname;
+  };
+  
+}//end namespace ubsens
+#endif
+/** @} */ // end of doxygen group 
+
