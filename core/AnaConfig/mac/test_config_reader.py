28791e3cd765cb21fb0b4a218e4d454a1e49868b
David Kaleko <kaleko@nevis.columbia.edu>
2014-10-15 11:14:07 -0500

    input argument to test reader script

--- core/AnaConfig/mac/test_config_reader.py
+++ core/AnaConfig/mac/test_config_reader.py
@@ -1,7 +1,13 @@
 from ROOT import *
+import sys
+
+if len(sys.argv) != 2:
+    print "USAGE: python test_config_reader.py name_of_config_file.config"
+    quit()
+
 gSystem.Load("libAnaConfig.so")
 
 cr = ubsens.config.ConfigReader()
-cr.SetConfigFile("test_config.config")
+cr.SetConfigFile(sys.argv[1])
 cr.Read()
 cr.Dump()
