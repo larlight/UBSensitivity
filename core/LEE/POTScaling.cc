db0f6e67e1adc4cdd809effa6af1d9671d3ccfc0
David Kaleko <kaleko@nevis.columbia.edu>
2014-12-08 14:31:55 -0500

    miniscule change to a cout in potscaling

--- core/LEE/POTScaling.cc
+++ core/LEE/POTScaling.cc
@@ -19,13 +19,13 @@ namespace ubsens{
     //If the config file has blank lines, use defaults
     if(_my_POT_string.empty()){
       std::string msg = "";
-      msg += class_name() + " is using default value for _my_POT_string.";
+      msg += class_name() + " is using default value (6.6) for _my_POT_string.";
       fMsg.send(::ubsens::fmwk::msg::kWARNING, __FUNCTION__, msg);
       _my_POT_string = "6.6";
     }
     if(_MINIBOONE_POT_string.empty()){
       std::string msg = "";
-      msg += class_name() + " is using default value for _MINIBOONE_POT_string.";
+      msg += class_name() + " is using default value (6.46) for _MINIBOONE_POT_string.";
       fMsg.send(::ubsens::fmwk::msg::kWARNING, __FUNCTION__, msg);
       _MINIBOONE_POT_string = "6.46";
     }
