c39e29ffc8aec60474cb68c6abfa36674d701c53
David Kaleko <kaleko@nevis.columbia.edu>
2014-12-09 10:30:55 -0500

    haven't committed this in ages, not important, just a debugging script

--- core/LEE/mac/test_xsecscaling.py
+++ core/LEE/mac/test_xsecscaling.py
@@ -3,7 +3,19 @@ import sys
 gSystem.Load("libLEE.so")
 
 
-x = XSecScaling()
+x = ubsens.XSecScaling()
+#x.SetMyInputFile("/Users/davidkaleko/Desktop/mindy/genie_ar40_nue_totcc_fullrange_lowEfinesample.root")
+x.SetMyInputFile("/Users/davidkaleko/Desktop/mindy/genie_CH2_nue_totcc_fullrange_lowEfinesample.root")
+#x.SetMBInputFile("/Users/davidkaleko/Desktop/mindy/genie_CH2_nue_totcc_fullrange_lowEfinesample.root")
+x.SetMBInputFile("/Users/davidkaleko/Data/LEE/tot_cc_genie_nuance_graphs.root")
+
+#x.SetMyInputGraphName("genie_nue_cctot_Ar_lowEfinesampling")
+x.SetMyInputGraphName("genie_nue_cctot_CH2_lowEfinesampling")
+#x.SetMBInputGraphName("genie_nue_cctot_CH2_lowEfinesampling")
+x.SetMBInputGraphName("nuance_total_cc_graph")
+
+x.SetMyNTargetsPerGram(ubsens.geo.MINIBOONE_TARGETS_PER_GRAM)
+x.SetMBNTargetsPerGram(ubsens.geo.MINIBOONE_TARGETS_PER_GRAM)
 x.LoadInputGraphs()
 x.ComputeXSecRatio()
 
